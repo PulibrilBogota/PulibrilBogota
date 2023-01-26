9e544b6961c33d64664d846ec574c518126a83d2
IsAboutLoan <123137486+PulibrilBogota@users.noreply.github.com>
2023-01-25 23:00:59 -0500

    P2

---
+++ src/pages/pulido-pisos-madera-servicio-precio-m2.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Layout from "../components/Layout";
+
+const PulidoPisosMadera = () => (
+  <Layout>
+    <div>
+      <h1>Pisos Madera</h1>
+      <p>Pisos Madera.</p>
+    </div>
+  </Layout>
+);
+
+export default PulidoPisosMadera;
