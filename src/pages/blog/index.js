38d835e2e6b9ee61625e6fdc909f51373884b0e4
IsAboutLoan <123137486+PulibrilBogota@users.noreply.github.com>
2023-01-26 02:10:02 -0500

    IMG

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -22,7 +22,7 @@ export default class BlogIndexPage extends React.Component {
               padding: "1rem",
             }}
           >
-            Últimos Artíciulos
+            Últimos Artículos
           </h1>
         </div>
         <section className="section">
--- src/pages/pulido-pisos-madera-servicio-precio-m2.js
+++ src/pages/pulido-pisos-madera-servicio-precio-m2.js
@@ -1,13 +1,34 @@
 import * as React from "react";
 import Layout from "../components/Layout";
-import FullWidthImage from "../components/FullWidthImage";
+import { StaticImage } from "gatsby-plugin-image"
 
 const PulidoPisosMadera = () => (
+
+  
   <Layout>
     <div>
       <h1>Pisos Madera</h1>
-      <p>Pisos Madera.</p>
     </div>
+                  <div className="columns">
+                    <div className="column is-12 has-text-centered">
+                      <a href="tel:3042775545" className="btn">
+                        Click para Llamar
+                      </a>
+                    </div>
+                  </div>
+
+
+
+
+    <StaticImage
+      src="../images/Logo-WhatsApp.png"
+      alt="WhatsApp"
+      placeholder="blurred"
+      layout="fixed"
+      width={1600}
+      height={309}
+    />
+      
   </Layout>
 );
 
